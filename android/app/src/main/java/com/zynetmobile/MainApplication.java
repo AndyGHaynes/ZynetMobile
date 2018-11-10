99a3bae6a1726cfff4c78085c9269c3827d096e7
Andy Haynes <andyghaynes@gmail.com>
2018-11-09 22:01:18 -0800

    add linear gradient dep

--- android/app/src/main/java/com/zynetmobile/MainApplication.java
+++ android/app/src/main/java/com/zynetmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.zynetmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
       );
