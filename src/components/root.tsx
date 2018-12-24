28b80c931252568a1f886b8bf43aa0de8e0e9faa
Andy Haynes <andyghaynes@gmail.com>
2018-12-23 19:08:54 -0800

    purge native-base themes

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,11 +1,5 @@
-import {
-  Container,
-  StyleProvider,
-} from 'native-base';
 import React, { PureComponent } from 'react';
 
-import getTheme from '../../native-base-theme/components';
-import theme from '../../native-base-theme/variables/custom';
 import Navigator from './navigator';
 
 type Props = {
@@ -19,16 +13,7 @@ export default class Root extends PureComponent<Props> {
 
   render() {
     return (
-      <StyleProvider style={getTheme(theme)}>
-        <Container
-          theme={theme}
-          style={{
-            backgroundColor: theme.defaultBackgroundColor,
-          }}
-        >
-          <Navigator />
-        </Container>
-      </StyleProvider>
+      <Navigator />
     );
   }
 }
